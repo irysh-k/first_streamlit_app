70ce9464c68ab6ccc5a5861c06992199c26e13d2
irysh-k <156081267+irysh-k@users.noreply.github.com>
2024-01-09 12:46:17 +0100

    Update streamlit_app.py
    updated text values

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.caption('Kale, Spinach & Pocket Smoothie')
-streamlit.caption('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Pocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
