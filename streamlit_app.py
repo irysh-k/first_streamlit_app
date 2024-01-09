358cd640da84ab107ed6c6bcb16331302ee92479
irysh-k <156081267+irysh-k@users.noreply.github.com>
2024-01-09 12:45:53 +0100

    Update streamlit_app.py
    text diff between text & caption

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.caption('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.caption('Kale, Spinach & Pocket Smoothie')
 streamlit.caption('Hard-Boiled Free-Range Egg')
 
