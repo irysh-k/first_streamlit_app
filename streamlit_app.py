c7090a2369a46e34f87cb453eb4b86d1323135e3
irysh-k <156081267+irysh-k@users.noreply.github.com>
2024-01-10 09:45:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,7 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice) 
     streamlit.dataframe(back_from_function)
 
-except URLError as e:
-  streamlit.stop()
+#except URLError as e: streamlit.stop()
 
 #connector to snowflake
 def get_fruit_load_list():
